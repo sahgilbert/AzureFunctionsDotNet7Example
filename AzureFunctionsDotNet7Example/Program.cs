af240cf465858569497d2b06c4b13db9ac20db0c
sahgilbert <sah.gilbert@gmail.com>
2024-02-07 13:51:24 +0000

    Add necessary nuget packages following StackOverflow question guidance

--- AzureFunctionsDotNet7Example/Program.cs
+++ AzureFunctionsDotNet7Example/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.Functions.Extensions;// Added this
+using Azure.Storage.Blobs;
+using Azure.Storage.Files.Shares;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Worker.Extensions.Http;
+using Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore;
+using Microsoft.Azure.Functions.Worker.Extensions.Storage.Queues;
+using Microsoft.Extensions.Azure;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
