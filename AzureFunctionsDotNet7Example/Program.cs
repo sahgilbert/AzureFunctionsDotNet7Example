f149bbdff1038aafefa48569dc2a01bf5aed1404
sahgilbert <sah.gilbert@gmail.com>
2024-02-07 13:22:33 +0000

    Initial commit

---
+++ AzureFunctionsDotNet7Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Functions.Extensions;// Added this
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
+
+host.Run();
+
---
+++ AzureFunctionsDotNet7Example/QueueTriggerFunctionExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsDotNet7Example
+{
+    public class QueueTriggerFunctionExample
+    {
+        private readonly ILogger<QueueTriggerFunctionExample> _logger;
+
+        public QueueTriggerFunctionExample(ILogger<QueueTriggerFunctionExample> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(QueueTriggerFunctionExample))]
+        public void Run([QueueTrigger("myqueue-items", Connection = "connection-string-goes-here")] QueueMessage message)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+        }
+    }
+}
+
